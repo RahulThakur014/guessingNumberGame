d7137d8c023d6ad138b7e987556a85844f8dd573
RahulThakur014 <rahulj.thakur.26@gmail.com>
2025-02-06 23:54:29 +0530

    Update miniproject.java

--- miniproject.java
+++ miniproject.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class miniproject {
 
-    static Scanner number = new Scanner(System.in); // getting input from user
+    int Scanner number = new Scanner(System.in); // getting input from user
     
         public static void main(String[] args) {
     
